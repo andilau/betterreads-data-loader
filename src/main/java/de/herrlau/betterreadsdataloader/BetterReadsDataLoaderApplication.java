a26f41af4f4545d59c1149573c05d78ebb540309
Andi Lau <andi@herrlau.de>
2021-10-26 11:19:14 +0200

    Load books into cassandra

--- src/main/java/de/herrlau/betterreadsdataloader/BetterReadsDataLoaderApplication.java
+++ src/main/java/de/herrlau/betterreadsdataloader/BetterReadsDataLoaderApplication.java
@@ -2,7 +2,10 @@ package de.herrlau.betterreadsdataloader;
 
 import de.herrlau.betterreadsdataloader.author.Author;
 import de.herrlau.betterreadsdataloader.author.AuthorRepository;
+import de.herrlau.betterreadsdataloader.book.Book;
+import de.herrlau.betterreadsdataloader.book.BookRepository;
 import de.herrlau.betterreadsdataloader.connection.DataStaxAstraProperties;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +21,11 @@ import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @SpringBootApplication
@@ -25,6 +33,8 @@ import java.util.stream.Stream;
 public class BetterReadsDataLoaderApplication {
     @Autowired
     AuthorRepository authorRepository;
+    @Autowired
+    BookRepository bookRepository;
 
     @Value("classpath:ol-dump-authors.txt")
     Resource authorsFile;
@@ -43,7 +53,7 @@ public class BetterReadsDataLoaderApplication {
 
     @PostConstruct
     public void start() {
-        loadAuthors();
+        //loadAuthors();
         loadWorks();
     }
 
@@ -81,10 +91,55 @@ public class BetterReadsDataLoaderApplication {
                         String json = line.substring(line.indexOf("{"));
                         try {
                             JSONObject jsonObject = new JSONObject(json);
-                            // construct author
+                            // construct book
+                            Book book = new Book();
+                            book.setId(jsonObject.getString("key").replace("/works/", ""));
+                            book.setName(jsonObject.optString("title"));
+
+                            JSONObject description = jsonObject.optJSONObject("description");
+                            if (description != null)
+                                book.setDescription(description.optString("value"));
+
+                            JSONObject published = jsonObject.getJSONObject("created");
+                            if (published != null) {
+                                var formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS");
+                                LocalDate date = LocalDate.parse(published.getString("value"),formatter);
+                                book.setPublishedDate(date);
+                            }
+
+                            JSONArray covers = jsonObject.optJSONArray("covers");
+                            List<String> coverIds = new ArrayList<>();
+                            for (int i = 0; covers != null && i < covers.length(); i++) {
+                                coverIds.add(covers.getString(i));
+                            }
+                            book.setCoverIds(coverIds);
+
+                            JSONArray authors = jsonObject.getJSONArray("authors");
+                            List<String> authorIds = new ArrayList<>();
+                            for (int i = 0; authors != null && i < authors.length(); i++) {
+                                String authorId = authors.getJSONObject(i)
+                                        .getJSONObject("author")
+                                        .getString("key")
+                                        .replace("/authors/", "");
+                                authorIds.add(authorId);
+                            }
+                            book.setAuthorIds(authorIds);
+
+                            var authorsList = authorIds.stream()
+                                    .map(id -> authorRepository.findById(id))
+                                    .map(author -> {
+                                                if (author.isPresent()) return author.get().getName();
+                                                else return "n/a";
+                                            }
+                                    )
+                                    .collect(Collectors.toList());
+                            book.setAuthorNames(authorsList);
+
+                            bookRepository.save(book);
+                            System.out.println("Loaded; " + book);
                         }
                         catch (JSONException e) {
-                            e.printStackTrace();
+                            System.err.println(e.getMessage());
                         }
                     }
             );
